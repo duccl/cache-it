bf822009973781f50e56f447bc067a96ec51a118
duccl <edr.ccol@gmail.com>
2022-05-15 19:36:03 -0300

    new AddCacheIt method overload

--- src/CacheIt/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/CacheIt/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using CacheIt.Hosting;
+using CacheIt.Options;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CacheIt.Extensions.DependencyInjection
@@ -10,5 +12,13 @@ namespace CacheIt.Extensions.DependencyInjection
             services.AddHostedService<Handler>();
             return services;
         }
+
+        public static IServiceCollection AddCacheIt(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHostedService<Handler>();
+            services.Configure<CustomRefreshOptions>(configuration.GetSection(CustomRefreshOptions.SectionName));
+            return services;
+        }
+
     }
 }
