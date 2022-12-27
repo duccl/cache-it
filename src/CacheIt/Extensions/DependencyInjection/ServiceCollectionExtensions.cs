871a5c093ff875f681272785b096c15ec2c5acdd
duccl <edr.ccol@gmail.com>
2022-12-27 17:57:51 -0300

    validating custom refresh

--- src/CacheIt/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/CacheIt/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
-﻿using CacheIt.Hosting;
+﻿using System.Configuration;
+using System.Linq;
+using System.Text;
+using CacheIt.Hosting;
 using CacheIt.Options;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,6 +10,26 @@ namespace CacheIt.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
+        private static void ValidateCustomRefreshOptions(IConfiguration configuration)
+        {
+            var customRefreshOptionsSection = configuration.GetSection(CustomRefreshOptions.SectionName);
+            var refreshTimesByCacheableName = customRefreshOptionsSection.GetSection("RefreshTimesByCacheableName").GetChildren();
+            var misconfiguredChildren = refreshTimesByCacheableName.Where(child => int.TryParse(child.Value, out _));
+            var areThereAnyMisconfiguredChildren = misconfiguredChildren.Any();
+
+            if (areThereAnyMisconfiguredChildren)
+            {
+                var builder = new StringBuilder();
+
+                foreach(var child in misconfiguredChildren)
+                    builder.AppendLine($"-> {child.Path}");
+
+                throw new ConfigurationErrorsException($"There are Cacheables configured with type 'int', this version is defined for timestamp configurations!"
+                    + $"{System.Environment.NewLine} Please review Your configurations for the following:{System.Environment.NewLine}"
+                    + $"{builder.ToString()}");
+            }
+        }
+
         public static IServiceCollection AddCacheIt(this IServiceCollection services)
         {
             services.AddHostedService<Handler>();
@@ -15,6 +38,8 @@ namespace CacheIt.Extensions.DependencyInjection
 
         public static IServiceCollection AddCacheIt(this IServiceCollection services, IConfiguration configuration)
         {
+            ValidateCustomRefreshOptions(configuration);
+            
             services.AddHostedService<Handler>();
             services.Configure<CustomRefreshOptions>(configuration.GetSection(CustomRefreshOptions.SectionName));
             return services;
